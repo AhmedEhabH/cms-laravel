1200049d0340b0b15c8d61a7c5ff38953f69b762
AhmedEhabH <ahmed.ehab.h@gmail.com>
2020-04-13 12:39:57 +0200

    finish Admin dashboard Update profile add request to verify update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Users\UpdateProfileRequest;
 use Illuminate\Http\Request;
 
 use App\User;
@@ -12,6 +13,25 @@ class UsersController extends Controller
     public function index()
     {
         return view('users.index')->with('users', User::all());
+    }
+
+    public function edit()
+    {
+        return view('users.edit')->with('user', auth()->user());
+    }
+
+    public function update(UpdateProfileRequest $request)
+    {
+        $user = auth()->user();
+
+        $user->update([
+            'name' => $request->name,
+            'about' => $request->about,
+        ]);
+
+        session()->flash('success', 'User updated successfully');
+
+        return redirect(route('users.index'));
     }
 
     public function makeAdmin(User $user)
