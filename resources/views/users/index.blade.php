a225edfae90eb2cbffcdc6129668f7d5560e5c49
AhmedEhabH <ahmed.ehab.h@gmail.com>
2020-04-13 12:03:48 +0200

    Admin panal Add Avatar Update composer

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,8 +19,7 @@
                     @foreach ($users as $user)
                         <tr>
                             <td>
-
-
+                                <img width="40px" height="40px" style="border-radius: 50%" src="{{ Gravatar::src($user->email) }}" alt="">
                             </td>
                             <td>
                                 {{ $user->name }}
